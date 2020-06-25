e9859485765460dac2226220b85d8aa9ac84f1f8
Gonzalo Gamez <takosaga@gmail.com>
2020-06-25 13:24:37 -0500

    Added career and bundle for JSON file

---
+++ CSCareerPaths/Career.swift
@@ -0,0 +1,28 @@
+//
+//  Career.swift
+//  CSCareerPaths
+//
+//  Created by Gonzalo Gamez on 6/25/20.
+//  Copyright © 2020 Gamez. All rights reserved.
+//
+
+import Foundation
+
+struct Career: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+    
+    static let allCareers: [Career] =
+        Bundle.main.decode("careers.json")
+    static let example = allCareers[0]
+}
+
