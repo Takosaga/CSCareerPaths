ad8e95e953b19775d228d6898522d0556e01d3a9
Takosaga <takosaga@gmail.com>
2020-07-09 19:35:25 -0500

    adding jobs.json with jobs class

---
+++ CSCareerPaths/Job.swift
@@ -0,0 +1,22 @@
+//
+//  Job.swift
+//  CSCareerPaths
+//
+//  Created by Gonzalo Gamez on 7/9/20.
+//  Copyright © 2020 Gamez. All rights reserved.
+//
+
+import Foundation
+
+struct Job: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+    let imageCredit: String
+    let average: Int
+    let short: String
+    
+    static let allJobs: [Job] =
+        Bundle.main.decode("jobs.JSON")
+    static let example = allJobs[0]
+}
