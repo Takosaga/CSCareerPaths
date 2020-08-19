29b1a51b947af97d38955fca91dbd61ee6d83e24
Takosaga <takosaga@gmail.com>
2020-08-19 00:24:46 -0500

    Adding pic and some of my teacher profile, moving to Xcode beta after this commit

--- CSCareerPaths/TeacherView.swift
+++ CSCareerPaths/TeacherView.swift
@@ -11,8 +11,18 @@ import SwiftUI
 struct TeacherView: View {
     var body: some View {
         NavigationView {
-            Text("About Teacher")
-        .navigationBarTitle("Teacher", displayMode: .inline)
+            ScrollView{
+                Image("mr.gamez")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 350, height: 350)
+                
+                Text("Mr. Gamez")
+                    .font(.headline)
+                Text("This is going to be my 7th year of teaching all years math and last two years computer science")
+                Text("What is different about me is that I learned all my computer science stuff through online in google and youtube. For example how to make this app and making my website and a youtube https://codewithgamez.github.io/website/")
+            }
+            .navigationBarTitle("Teacher", displayMode: .inline)
         }
     }
 }
