f55b4cdf822f0a405c9255ba334dc8167bf04254
Takosaga <takosaga@gmail.com>
2020-09-28 13:06:50 -0500

    Finished rest info of jobs, adjust layout of teacher to scroll view and career pic

--- CSCareerPaths/TeacherView.swift
+++ CSCareerPaths/TeacherView.swift
@@ -12,38 +12,41 @@ import AVKit
 struct TeacherView: View {
     var body: some View {
         NavigationView {
-            
-            VStack(alignment: .center){
-                Image("mr.gamez")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
-                
-                Text("Mr. Gamez")
+            ScrollView(.vertical){
+                VStack(alignment: .center){
+                    Image("mr.gamez")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
                     
-                    .font(.headline)
-                
-                Text("This is going to be my 7th year of teaching all years math and last two years computer science. What is different about me is that I learned all my computer science stuff through online in google and youtube. For example I learned how to make this app, to make a website for my online course and a youtube.")
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.secondary)
+                    Text("Mr. Gamez")
+                        
+                        .font(.headline)
                     
-                
-                
-                Link("My Website", destination: URL(string: "https://codewithgamez.github.io/website/")!)
-                    .padding()
-                
-                Link("My YouTube", destination: URL(string: "https://www.youtube.com/channel/UCq5m_ARYP0LF3vfeArU8dyg")!)
-                    .padding()
-                
-                Text("Special Thanks to")
-                    .font(.headline)
-                
-                Text("🗿Isela Herrera: former student who helped search info \n 💻dontworryimnotacop from reddit: posting various careers in CS \n 👨‍🏫 Paul Hudson: helped me learn swiftUI with his free course - Hacking with Swift")
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.secondary)
+                    Text("This is going to be my 7th year of teaching all years math and last two years computer science. What is different about me is that I learned all my computer science stuff through online in google and youtube. For example I learned how to make this app, to make a website for my online course and a youtube.")
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.secondary)
+                        
                     
-               
-                Spacer()
+                    
+                    Link("My Website", destination: URL(string: "https://codewithgamez.github.io/website/")!)
+                        .padding()
+                        
+                    
+                    Link("My YouTube", destination: URL(string: "https://www.youtube.com/channel/UCq5m_ARYP0LF3vfeArU8dyg")!)
+                        .padding()
+                        
+                    
+                    Text("Special Thanks to")
+                        .font(.headline)
+                    
+                    Text("🗿Isela Herrera: former student who helped search info \n 💻dontworryimnotacop from reddit: posting various careers in CS \n 👨‍🏫 Paul Hudson: helped me learn swiftUI with his free course - Hacking with Swift")
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.secondary)
+                        
+                   
+                    Spacer()
+                }
             }
            
             .navigationBarTitle("Teacher", displayMode: .inline)
